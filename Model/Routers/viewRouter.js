121d8c04b8356c701e276dc8f07cf3062430e063
Srikaar <94304760+k-Srikaar@users.noreply.github.com>
2024-02-27 17:01:24 +0530

    Delete Model/Routers directory

--- Model/Routers/viewRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const viewControllers = require('./../controllers/viewControllers');
-const router = express.Router();
-
-router.get('/', viewControllers.getOverview);
-router.get('/tour/:slug', viewControllers.getTour);
-router.get('/login', viewControllers.getLoginForm);
-module.exports = router;
